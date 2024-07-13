d8c396d80a65e9c0deebdde7892c8b0ae2f02c97
Gavin Shaw <gavshaw+github@gmail.com>
2024-07-13 15:26:23 -0400

    Add textfield for location and api call to get weather

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import React, { useState } from 'react';
+
+const api = {
+    baseUrl: "https://api.openweathermap.org/data/2.5/",
+    key: ""
+};
+
 function App() {
 
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState('');
+
+    const search = event => {
+        if (event.key === 'Enter') {
+            fetch(`${api.baseUrl}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(reponse => reponse.json())
+                .then(result => {
+                    setWeather(result);
+                    setQuery('');
+                });
+        }
+    };
+
     return (
-        <div></div>
+        <div>
+            <main>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        onChange={e => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    />
+                </div>
+                {(typeof weather.main != 'undefined') ? (
+                    <div>
+
+                    </div>
+                ) : ('')}
+            </main>
+        </div>
     );
 }
 
